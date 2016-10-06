1f5ceb10a864be165573d426c13c9f37c02006d4
Cuttle Dev <cuttlefysh@Cuttles-Mac.local>
2016-10-06 00:00:21 -0500

    Work started on con version

---
+++ Pipian/Pipian/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pipian
+//
+//  Created by Cuttle Dev on 10/5/16.
+//  Copyright © 2016 Cuttlefysh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
